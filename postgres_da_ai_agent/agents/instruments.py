88fc5fe28fa42aaf2a62dbb8fd7383efdf2296b5
qwukker <minor7addfloortom@gmail.com>
2023-11-04 15:34:49 -0500

    init v6

---
+++ postgres_da_ai_agent/agents/instruments.py
@@ -0,0 +1,155 @@
+from postgres_da_ai_agent.modules.db import PostgresManager
+from postgres_da_ai_agent.modules import file
+import os
+
+BASE_DIR = os.environ.get("BASE_DIR", "./agent_results")
+
+
+class AgentInstruments:
+    """
+    Base class for multli-agent instruments that are tools, state, and functions that an agent can use across the lifecycle of conversations
+    """
+
+    def __init__(self) -> None:
+        self.session_id = None
+        self.messages = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+    def sync_messages(self, messages: list):
+        """
+        Syncs messages with the orchestrator
+        """
+        raise NotImplementedError
+
+    def make_agent_chat_file(self, team_name: str):
+        return os.path.join(self.root_dir, f"agent_chats_{team_name}.json")
+
+    @property
+    def root_dir(self):
+        return os.path.join(BASE_DIR, self.session_id)
+
+
+class PostgresAgentInstruments(AgentInstruments):
+    """
+    Unified Toolset for the Postgres Data Analytics Multi-Agent System
+
+    Advantages:
+        - All agents have access to the same state and functions
+        - Gives agent functions awareness of changing context
+        - Clear and concise capabilities for agents
+        - Clean database connection management
+
+    Guidelines:
+        - Agent Functions should not call other agent functions directly
+            - Instead Agent Functions should call external lower level modules
+        - Prefer 1 to 1 mapping of agents and their functions (controverisal)
+        - The state lifecycle lives between all agent orchestrations
+    """
+
+    def __init__(self, db_url: str, session_id: str) -> None:
+        super().__init__()
+
+        self.db_url = db_url
+        self.db = None
+        self.session_id = session_id
+        self.messages = []
+        self.complete_keyword = "APPROVED"
+
+        self.innovation_index = 0
+
+    def __enter__(self):
+        self.reset_files()
+        self.db = PostgresManager()
+        self.db.connect_with_url(self.db_url)
+        return self, self.db
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.db.close()
+
+    def sync_messages(self, messages: list):
+        self.messages = messages
+
+    def reset_files(self):
+        """
+        Clear everything in the root_dir
+        """
+
+        # if it does not exist create it
+        if not os.path.exists(self.root_dir):
+            os.makedirs(self.root_dir)
+
+        for fname in os.listdir(self.root_dir):
+            os.remove(os.path.join(self.root_dir, fname))
+
+    def get_file_path(self, fname: str):
+        return os.path.join(self.root_dir, fname)
+
+    # -------------------------- Agent Properties -------------------------- #
+
+    @property
+    def run_sql_results_file(self):
+        return self.get_file_path("run_sql_results.json")
+
+    # -------------------------- Agent Functions -------------------------- #
+
+    def run_sql(self, sql: str) -> str:
+        """
+        Run a SQL query against the postgres database
+        """
+        results_as_json = self.db.run_sql(sql)
+
+        fname = self.run_sql_results_file
+
+        # dump these results to a file
+        with open(fname, "w") as f:
+            f.write(results_as_json)
+
+        return "Successfully delivered results to json file"
+
+    def validate_run_sql(self):
+        """
+        validate that the run_sql results file exists and has content
+        """
+        fname = self.run_sql_results_file
+
+        with open(fname, "r") as f:
+            content = f.read()
+
+        if not content:
+            return False, f"File {fname} is empty"
+
+        return True, ""
+
+    def write_file(self, content: str):
+        fname = self.get_file_path(f"write_file.txt")
+        return file.write_file(fname, content)
+
+    def write_json_file(self, json_str: str):
+        fname = self.get_file_path(f"write_json_file.json")
+        return file.write_json_file(fname, json_str)
+
+    def write_yml_file(self, json_str: str):
+        fname = self.get_file_path(f"write_yml_file.yml")
+        return file.write_yml_file(fname, json_str)
+
+    def write_innovation_file(self, content: str):
+        fname = self.get_file_path(f"{self.innovation_index}_innovation_file.json")
+        file.write_file(fname, content)
+        self.innovation_index += 1
+        return f"Successfully wrote innovation file. You can check my work."
+
+    def validate_innovation_files(self):
+        # loop from 0 to innovation_index and verify file exists with content
+        for i in range(self.innovation_index):
+            fname = self.get_file_path(f"{i}_innovation_file.json")
+            with open(fname, "r") as f:
+                content = f.read()
+                if not content:
+                    return False, f"File {fname} is empty"
+
+        return True, ""
---
+++ postgres_da_ai_agent/modules/embeddings.py
@@ -0,0 +1,76 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import BertTokenizer, BertModel
+
+
+class DatabaseEmbedder:
+    def __init__(self):
+        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.model = BertModel.from_pretrained("bert-base-uncased")
+        self.map_name_to_embeddings = {}
+        self.map_name_to_table_def = {}
+
+    def add_table(self, table_name: str, text_representation: str):
+        self.map_name_to_embeddings[table_name] = self.compute_embeddings(
+            text_representation
+        )
+
+        self.map_name_to_table_def[table_name] = text_representation
+
+    def compute_embeddings(self, text):
+        inputs = self.tokenizer(
+            text, return_tensors="pt", truncation=True, padding=True, max_length=512
+        )
+        outputs = self.model(**inputs)
+        return outputs["pooler_output"].detach().numpy()
+
+    def get_similar_tables_via_embeddings(self, query, n=3):
+        """
+        Given a query, find the top 'n' tables that are most similar to it.
+
+        Args:
+        - query (str): The user's natural language query.
+        - n (int, optional): Number of top tables to return. Defaults to 3.
+
+        Returns:
+        - list: Top 'n' table names ranked by their similarity to the query.
+        """
+        # Compute the embedding for the user's query
+        query_embedding = self.compute_embeddings(query)
+        # Calculate cosine similarity between the query and all tables
+        similarities = {
+            table: cosine_similarity(query_embedding, emb)[0][0]
+            for table, emb in self.map_name_to_embeddings.items()
+        }
+        # Rank tables based on their similarity scores and return top 'n'
+        return sorted(similarities, key=similarities.get, reverse=True)[:n]
+
+    def get_similar_table_names_via_word_match(self, query: str):
+        """
+        if any word in our query is a table name, add the table to a list
+        """
+
+        tables = []
+
+        for table_name in self.map_name_to_table_def.keys():
+            if table_name.lower() in query.lower():
+                tables.append(table_name)
+
+        return tables
+
+    def get_similar_tables(self, query: str, n=3):
+        """
+        combines results from get_similar_tables_via_embeddings and get_similar_table_names_via_word_match
+        """
+
+        similar_tables_via_embeddings = self.get_similar_tables_via_embeddings(query, n)
+        similar_tables_via_word_match = self.get_similar_table_names_via_word_match(
+            query
+        )
+
+        return similar_tables_via_embeddings + similar_tables_via_word_match
+
+    def get_table_definitions_from_names(self, table_names: list) -> list:
+        table_defs = [
+            self.map_name_to_table_def[table_name] for table_name in table_names
+        ]
+        return "\n\n".join(table_defs)
