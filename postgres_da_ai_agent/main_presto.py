ecf775dbd0dc5a703daf399ad3ce4081e58ccbd8
alvropena <me@alvropena.com>
2023-12-22 11:02:52 -0500

    Modifying db_presto.py's queries and connections to fit PrestoDB cluster

--- postgres_da_ai_agent/main_presto.py
+++ postgres_da_ai_agent/main_presto.py
@@ -4,7 +4,7 @@ Heads up: in v7 pyautogen doesn't work with the latest openai version so this fi
 
 import os
 from postgres_da_ai_agent.agents.instruments import PrestoAgentInstruments
-from postgres_da_ai_agent.modules.db_postgres import PrestoManager
+from postgres_da_ai_agent.modules.db_presto import PrestoManager
 from postgres_da_ai_agent.modules import llm
 from postgres_da_ai_agent.modules import orchestrator
 from postgres_da_ai_agent.modules import rand
@@ -22,19 +22,19 @@ from postgres_da_ai_agent.data_types import ConversationResult
 
 dotenv.load_dotenv()
 
-assert os.environ.get("PRESTO_DATABASE_URL"), "PRESTO_DATABASE_URL not found in .env file"
+assert os.environ.get("PRESTO_DATABASE_URL"), "PRESTO_CONNECTION_URL not found in .env file"
 assert os.environ.get(
     "OPENAI_API_KEY"
-), "OPENAI_API_KEY not found in .env file"
+), "OPENAI_CONNECTION_URL not found in .env file"
 
 
 # ---------------- Constants ---------------------------------
 
 
-PRESTO_DATABASE_URL = os.environ.get("PRESTO_DATABASE_URL")
+PRESTO_DB_URL = os.environ.get("PRESTO_DATABASE_URL")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-PRESTO_DATABASE_TABLE_DEFINITIONS_CAP_REF = "PRESTO_DATABASE_TABLE_DEFINITIONS"
+PRESTO_TABLE_DEFINITIONS_CAP_REF = "TABLE_DEFINITIONS"
 
 
 def main():
@@ -56,8 +56,6 @@ def main():
 
     # ---------------- Create Agent Instruments And Build Database Connection ----------------
 
-
-        # -------- BUILD TABLE DEFINITIONS -----------
     with PrestoAgentInstruments(DB_URL, session_id) as (agent_instruments, db):
         # ----------- Gate Team: Prevent bad prompts from running and burning your $$$ -------------
 
@@ -85,11 +83,100 @@ def main():
                 print("❌ Gate Team Rejected - Invalid response")
                 return
 
+        # -------- BUILD TABLE DEFINITIONS -----------
+
+        map_table_name_to_table_def = db.get_table_definition_map_for_embeddings()
+
+        database_embedder = embeddings.DatabaseEmbedder()
+
+        for name, table_def in map_table_name_to_table_def.items():
+            database_embedder.add_table(name, table_def)
+
+        similar_tables = database_embedder.get_similar_tables(raw_prompt, n=5)
+
+        table_definitions = database_embedder.get_table_definitions_from_names(
+            similar_tables
+        )
+
+        related_table_names = db.get_related_tables(similar_tables, n=3)
+
+        core_and_related_table_definitions = (
+            database_embedder.get_table_definitions_from_names(
+                related_table_names + similar_tables
+            )
+        )
+
+        prompt = llm.add_cap_ref(
+            prompt,
+            f"Use these {POSTGRES_TABLE_DEFINITIONS_CAP_REF} to satisfy the database query.",
+            POSTGRES_TABLE_DEFINITIONS_CAP_REF,
+            table_definitions,
+        )
+
         # ----------- Data Eng Team: Based on a SQL table definitions and a prompt create an sql statement and execute it -------------
 
+        data_eng_orchestrator = agents.build_team_orchestrator(
+            "data_eng",
+            agent_instruments,
+            validate_results=agent_instruments.validate_run_sql,
+        )
+
+        data_eng_conversation_result: ConversationResult = (
+            data_eng_orchestrator.sequential_conversation(prompt)
+        )
+
+        match data_eng_conversation_result:
+            case ConversationResult(
+                success=True, cost=data_eng_cost, tokens=data_eng_tokens
+            ):
+                print(
+                    f"✅ Orchestrator was successful. Team: {data_eng_orchestrator.name}"
+                )
+                print(
+                    f"💰📊🤖 {data_eng_orchestrator.name} Cost: {data_eng_cost}, tokens: {data_eng_tokens}"
+                )
+            case _:
+                print(
+                    f"❌ Orchestrator failed. Team: {data_eng_orchestrator.name} Failed"
+                )
 
         # ----------- Data Insights Team: Based on sql table definitions and a prompt generate novel insights -------------
 
+        innovation_prompt = f"Given this database query: '{raw_prompt}'. Generate novel insights and new database queries to give business insights."
+
+        insights_prompt = llm.add_cap_ref(
+            innovation_prompt,
+            f"Use these {POSTGRES_TABLE_DEFINITIONS_CAP_REF} to satisfy the database query.",
+            POSTGRES_TABLE_DEFINITIONS_CAP_REF,
+            core_and_related_table_definitions,
+        )
+
+        data_insights_orchestrator = agents.build_team_orchestrator(
+            "data_insights",
+            agent_instruments,
+            validate_results=agent_instruments.validate_innovation_files,
+        )
+
+        data_insights_conversation_result: ConversationResult = (
+            data_insights_orchestrator.round_robin_conversation(
+                insights_prompt, loops=1
+            )
+        )
+
+        match data_insights_conversation_result:
+            case ConversationResult(
+                success=True, cost=data_insights_cost, tokens=data_insights_tokens
+            ):
+                print(
+                    f"✅ Orchestrator was successful. Team: {data_insights_orchestrator.name}"
+                )
+                print(
+                    f"💰📊🤖 {data_insights_orchestrator.name} Cost: {data_insights_cost}, tokens: {data_insights_tokens}"
+                )
+            case _:
+                print(
+                    f"❌ Orchestrator failed. Team: {data_insights_orchestrator.name} Failed"
+                )
 
 
 if __name__ == "__main__":
--- postgres_da_ai_agent/modules/db_presto.py
+++ postgres_da_ai_agent/modules/db_presto.py
@@ -1,11 +1,10 @@
-from datetime import datetime
 import json
-import psycopg2
-from psycopg2.sql import SQL, Identifier
+import prestodb
+from datetime import datetime
 
 class PrestoManager:
     """
-    A class to manage postgres connections and queries
+    A class to manage PrestoDB connections and queries
     """
 
     def __init__(self):
@@ -16,13 +15,28 @@ class PrestoManager:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.cur:
-            self.cur.close()
-        if self.conn:
-            self.conn.close()
+        self.close()
 
     def connect_with_url(self, url):
-        self.conn = psycopg2.connect(url)
+        # Extract connection parameters from the URL
+        # Assuming the URL format: https://user:password@host:port/catalog/schema
+        _, rest = url.split("://")
+        user_password, host_port_catalog_schema = rest.split("@")
+        user, password = user_password.split(":")
+        host_port, catalog_schema = host_port_catalog_schema.split("/")
+        host, port = host_port.split(":")
+        catalog, schema = catalog_schema.split("/")
+
+        # Establishing the connection
+        self.conn = prestodb.dbapi.connect(
+            host=host,
+            port=int(port),
+            user=user,
+            catalog=catalog,
+            schema=schema,
+            http_scheme='https',
+            auth=prestodb.auth.BasicAuthentication(user, password)
+        )
         self.cur = self.conn.cursor()
 
     def close(self):
@@ -33,16 +47,20 @@ class PrestoManager:
 
     def run_sql(self, sql) -> str:
         """
-        Run a SQL query against the postgres database
+        Run a SQL query against the PrestoDB database
         """
         self.cur.execute(sql)
-        columns = [desc[0] for desc in self.cur.description]
-        res = self.cur.fetchall()
+        rows = self.cur.fetchall()
 
-        list_of_dicts = [dict(zip(columns, row)) for row in res]
+        # Handling empty result sets
+        if not rows:
+            return json.dumps([])
+
+        # Fetching column names from the cursor
+        columns = [desc[0] for desc in self.cur.description]
+        list_of_dicts = [dict(zip(columns, row)) for row in rows]
 
         json_result = json.dumps(list_of_dicts, indent=4, default=self.datetime_handler)
-
         return json_result
 
     def datetime_handler(self, obj):
@@ -51,114 +69,38 @@ class PrestoManager:
         """
         if isinstance(obj, datetime):
             return obj.isoformat()
-        return str(obj)  # or just return the object unchanged, or another default value
-
-    def get_table_definition(self, table_name):
-        """
-        Generate the 'create' definition for a table
-        """
-
-        get_def_stmt = """
-        SELECT pg_class.relname as tablename,
-            pg_attribute.attnum,
-            pg_attribute.attname,
-            format_type(atttypid, atttypmod)
-        FROM pg_class
-        JOIN pg_namespace ON pg_namespace.oid = pg_class.relnamespace
-        JOIN pg_attribute ON pg_attribute.attrelid = pg_class.oid
-        WHERE pg_attribute.attnum > 0
-            AND pg_class.relname = %s
-            AND pg_namespace.nspname = 'public'  -- Assuming you're interested in public schema
-        """
-        self.cur.execute(get_def_stmt, (table_name,))
-        rows = self.cur.fetchall()
-        create_table_stmt = "CREATE TABLE {} (\n".format(table_name)
-        for row in rows:
-            create_table_stmt += "{} {},\n".format(row[2], row[3])
-        create_table_stmt = create_table_stmt.rstrip(",\n") + "\n);"
-        return create_table_stmt
+        return str(obj)
 
     def get_all_table_names(self):
         """
-        Get all table names in the database
+        Get all table names in the PrestoDB database
         """
-        get_all_tables_stmt = (
-            "SELECT tablename FROM pg_tables WHERE schemaname = 'public';"
-        )
-        self.cur.execute(get_all_tables_stmt)
+        # Adjusted query for PrestoDB
+        self.cur.execute("SHOW TABLES")
         return [row[0] for row in self.cur.fetchall()]
+
+    def get_table_definition(self, table_name):
+        """
+        Get the 'create' definition for a table in PrestoDB
+        """
+        # PrestoDB does not directly support a single query to get the full create statement.
+        # You need to construct it manually from column information.
+        self.cur.execute(f"DESCRIBE {table_name}")
+        rows = self.cur.fetchall()
+        create_table_stmt = f"CREATE TABLE {table_name} (\n"
+        for row in rows:
+            column_name, column_type, _ = row[:3]
+            create_table_stmt += f"  {column_name} {column_type},\n"
+        create_table_stmt = create_table_stmt.rstrip(",\n") + "\n);"
+        return create_table_stmt
 
     def get_table_definitions_for_prompt(self):
         """
-        Get all table 'create' definitions in the database
+        Get all table 'create' definitions in the PrestoDB database
         """
         table_names = self.get_all_table_names()
         definitions = []
         for table_name in table_names:
             definitions.append(self.get_table_definition(table_name))
         return "\n\n".join(definitions)
-
-    def get_table_definition_map_for_embeddings(self):
-        """
-        Creates a map of table names to table definitions
-        """
-        table_names = self.get_all_table_names()
-        definitions = {}
-        for table_name in table_names:
-            definitions[table_name] = self.get_table_definition(table_name)
-        return definitions
-
-    def get_related_tables(self, table_list, n=2):
-        """
-        Get tables that have foreign keys referencing the given table
-        """
-
-        related_tables_dict = {}
-
-        for table in table_list:
-            # Query to fetch tables that have foreign keys referencing the given table
-            self.cur.execute(
-                """
-                SELECT 
-                    a.relname AS table_name
-                FROM 
-                    pg_constraint con 
-                    JOIN pg_class a ON a.oid = con.conrelid 
-                WHERE 
-                    confrelid = (SELECT oid FROM pg_class WHERE relname = %s)
-                LIMIT %s;
-                """,
-                (table, n),
-            )
-
-            related_tables = [row[0] for row in self.cur.fetchall()]
-
-            # Query to fetch tables that the given table references
-            self.cur.execute(
-                """
-                SELECT 
-                    a.relname AS referenced_table_name
-                FROM 
-                    pg_constraint con 
-                    JOIN pg_class a ON a.oid = con.confrelid 
-                WHERE 
-                    conrelid = (SELECT oid FROM pg_class WHERE relname = %s)
-                LIMIT %s;
-                """,
-                (table, n),
-            )
-
-            related_tables += [row[0] for row in self.cur.fetchall()]
-
-            related_tables_dict[table] = related_tables
-
-        # convert dict to list and remove dups
-        related_tables_list = []
-        for table, related_tables in related_tables_dict.items():
-            related_tables_list += related_tables
-
-        related_tables_list = list(set(related_tables_list))
-
-        return related_tables_list
-
 
